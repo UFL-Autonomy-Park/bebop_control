a11b5b1b97f0e8f5527ba61fd8bf8e02db3b0265
Max Gardenswartz <max.gardenswartz@proton.me>
2026-04-28 19:09:12 -0400

    chore: clean up and add license

--- src/bebop_control_node.cpp
+++ src/bebop_control_node.cpp
@@ -39,32 +39,26 @@ public:
         dt_ = 1.0 / control_freq_hz_;
         max_tilt_angle_rad_ = max_tilt_angle_deg_ * M_PI / 180.0;
 
-        // Subscribers
-        // Odometry (Odometry)
         odom_sub_ = this->create_subscription<OdomMsg>(
             "filtered_odom",
             rclcpp::SensorDataQoS(),
             std::bind(&BebopControlNode::odomCallback, this, std::placeholders::_1)
         );
-        // Desired velocity (Twist)
         des_vel_sub_ = this->create_subscription<TwistMsg>(
             "cmd_vel_des",
-            10, // Default QoS with N=10 queue
+            10,
             std::bind(&BebopControlNode::desVelCallback, this, std::placeholders::_1)
         );
-        // Bebop mode (Int32)
         bebop_mode_sub_ = this->create_subscription<std_msgs::msg::Int32>(
             "mode",
             10,
             std::bind(&BebopControlNode::bebopModeCallback, this, std::placeholders::_1)
         );
-        // Publishers (Twist)
         cmd_vel_pub_ = this->create_publisher<TwistMsg>(
             "cmd_vel",
             10
         );
 
-        // Log
         RCLCPP_INFO_STREAM(this->get_logger(), "Low-level Bebop controller active. Listening for cmd_vel");
     }
 
@@ -78,13 +72,12 @@ private:
     // State
     TwistMsg target_vel_world_; // Desired veloicty in the world/mocap frame
     Eigen::Vector3d current_vel_body_; // Current vel in the body frame
-    Eigen::Quaterniond current_att_; // Orientation in the world/mocap frame
+    Eigen::Quaterniond current_att_world_; // Orientation in the world/mocap frame
     rclcpp::Time last_odom_time_;
     rclcpp::Time last_cmd_time_;
     bool odom_received_ = false;
     bool cmd_received_ = false;
 
-    // Integator setup
     double err_sum_x_ = 0.0;
     double err_sum_y_ = 0.0;
     bool integrator_on_x_ = true;
@@ -92,12 +85,99 @@ private:
     bool is_saturated_x_ = false;
     bool is_saturated_y_ = false;
 
-    // Parameters
     double max_tilt_angle_rad_;
     double max_vertical_speed_mps_;
     double kp_xy_, ki_xy_;
     double dt_;
-    FlightMode current_mode_ = FlightMode::TELEOP; // 0 means pilot can manually control; 1 means off-board/autonomy mode
+    FlightMode current_mode_ = FlightMode::TELEOP;
+
+    rclcpp::Subscription<OdomMsg>::SharedPtr odom_sub_;
+    rclcpp::Subscription<TwistMsg>::SharedPtr des_vel_sub_;
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr bebop_mode_sub_;
+    rclcpp::Publisher<TwistMsg>::SharedPtr cmd_vel_pub_;
+
+    void controlLoop() {
+        if (!odom_received_ || (this->now() - last_odom_time_) >= rclcpp::Duration::from_seconds(STALE_ODOM_TIMEOUT_SEC)) {
+            stopDrone();
+            RCLCPP_WARN(this->get_logger(), "Stale odometry.");
+            return;
+        }
+
+        if (current_mode_ != FlightMode::OFFBOARD) {
+            err_sum_x_ = 0.0;
+            err_sum_y_ = 0.0;
+            RCLCPP_WARN(this->get_logger(), "Not in offboard mode.");
+            return;
+        }
+
+        // Active transform from world/mocap frame to body frame
+        current_att_world_.normalize(); 
+        Eigen::Vector3d target_vel_body = current_att_world_.inverse() * Eigen::Vector3d(
+            target_vel_world_.linear.x,
+            target_vel_world_.linear.y,
+            target_vel_world_.linear.z
+        );
+
+        // Calculate errors (body frame)
+        double err_x = target_vel_body.x() - current_vel_body_.x();
+        double err_y = target_vel_body.y() - current_vel_body_.y();
+        int sgn_error_x = sgn(err_x);
+        int sgn_error_y = sgn(err_y);
+
+        // Integral (assumes a ZOH)
+        if (integrator_on_x_) {
+            err_sum_x_ = err_sum_x_ + err_x * dt_;
+        }
+        
+        if (integrator_on_y_) {
+            err_sum_y_ = err_sum_y_ + err_y * dt_;
+        }
+        
+        // PI output (desired tilt in rad)
+        double u_pitch = (kp_xy_ * err_x + ki_xy_ * err_sum_x_);
+        double u_roll  = (kp_xy_ * err_y + ki_xy_ * err_sum_y_);
+        int sgn_u_pitch = sgn(u_pitch);
+        int sgn_u_roll = sgn(u_roll);
+
+        // Anti-windup
+        bool sgn_in_matches_sgn_out_X = (sgn_error_x == sgn_u_pitch);
+        bool sgn_in_matches_sgn_out_Y = (sgn_error_y == sgn_u_roll);
+        double u_pitch_sat = std::clamp(u_pitch / max_tilt_angle_rad_, -1.0, 1.0);
+        double u_roll_sat = std::clamp(u_roll / max_tilt_angle_rad_, -1.0, 1.0);
+        is_saturated_x_ = (std::abs(u_pitch) >= max_tilt_angle_rad_);
+        is_saturated_y_ = (std::abs(u_roll) >= max_tilt_angle_rad_);
+        integrator_on_x_ = !(is_saturated_x_ && sgn_in_matches_sgn_out_X);
+        integrator_on_y_ = !(is_saturated_y_ && sgn_in_matches_sgn_out_Y);
+        
+        TwistMsg cmd_vel;
+        cmd_vel.linear.x = u_pitch_sat;
+        cmd_vel.linear.y = u_roll_sat;
+        cmd_vel.linear.z = std::clamp(target_vel_body.z() / max_vertical_speed_mps_,-1.0, 1.0);
+        cmd_vel.angular.z = std::clamp(target_vel_world_.angular.y, -1.0, 1.0); 
+        
+        if ( !std::isfinite(cmd_vel.linear.x) || 
+             !std::isfinite(cmd_vel.linear.y) || 
+             !std::isfinite(cmd_vel.linear.z) || 
+             !std::isfinite(cmd_vel.angular.z) ) 
+        {
+            RCLCPP_FATAL(this->get_logger(), "Controller produced infinite cmd_vel");
+            throw std::runtime_error("Controller produced infinite cmd_vel");
+        } else {
+            cmd_vel_pub_->publish(cmd_vel);
+        }
+    }
+
+    void stopDrone() {
+        err_sum_x_ = 0.0;
+        err_sum_y_ = 0.0;
+
+        TwistMsg cmd_vel_zero;
+        cmd_vel_zero.linear.x = 0.0;
+        cmd_vel_zero.linear.y = 0.0;
+        cmd_vel_zero.linear.z = 0.0;
+        cmd_vel_zero.angular.z = 0.0;
+        cmd_vel_pub_->publish(cmd_vel_zero);
+    }
 
     void desVelCallback(const TwistMsg::SharedPtr msg) {
         last_cmd_time_ = this->now();
@@ -111,7 +191,6 @@ private:
         last_odom_time_ = this->now();
         odom_received_ = true;
 
-        // Check for finite values
         if ( !std::isfinite(msg->twist.twist.linear.x) || 
              !std::isfinite(msg->twist.twist.linear.y) || 
              !std::isfinite(msg->twist.twist.linear.z) || 
@@ -143,7 +222,7 @@ private:
             msg->twist.twist.linear.y,
             msg->twist.twist.linear.z
         );
-        current_att_ = Eigen::Quaterniond(
+        current_att_world_ = Eigen::Quaterniond(
             msg->pose.pose.orientation.w,
             msg->pose.pose.orientation.x,
             msg->pose.pose.orientation.y,
@@ -152,7 +231,6 @@ private:
     }
 
     void bebopModeCallback(const std_msgs::msg::Int32::SharedPtr msg) {
-        // Check if we are switching from teleop (0) into offboard (1)
         if (current_mode_ == FlightMode::TELEOP && msg->data == static_cast<int>(FlightMode::OFFBOARD))
         {
             RCLCPP_INFO(this->get_logger(), "Switching to OFFBOARD, freshening control time stamp");
@@ -162,106 +240,6 @@ private:
         }
         current_mode_ = static_cast<FlightMode>(msg->data);
     }
-
-    void controlLoop() {
-        // Hover if no odom (either never received or stale)
-        if (!odom_received_ || (this->now() - last_odom_time_) >= rclcpp::Duration::from_seconds(STALE_ODOM_TIMEOUT_SEC)) {
-            stopDrone();
-            RCLCPP_WARN(this->get_logger(), "Stale odometry.");
-            return;
-        }
-
-        // Hover if not in offboard mode
-        if (current_mode_ != FlightMode::OFFBOARD) {
-            // Reset integral terms
-            err_sum_x_ = 0.0;
-            err_sum_y_ = 0.0;
-            RCLCPP_WARN(this->get_logger(), "Not in offboard mode.");
-            return;
-        }
-
-        // Coordinate transform
-        current_att_.normalize(); // Active transform from world/mocap frame to body frame
-        Eigen::Vector3d target_vel_body = current_att_.inverse() * Eigen::Vector3d(
-            target_vel_world_.linear.x,
-            target_vel_world_.linear.y,
-            target_vel_world_.linear.z
-        );
-
-        // Calculate errors (body frame)
-        double err_x = target_vel_body.x() - current_vel_body_.x();
-        double err_y = target_vel_body.y() - current_vel_body_.y();
-        int sgn_error_x = sgn(err_x);
-        int sgn_error_y = sgn(err_y);
-
-        // Integral (assumes a ZOH)
-        if (integrator_on_x_) {
-            err_sum_x_ = err_sum_x_ + err_x * dt_;
-        }
-        
-        if (integrator_on_y_) {
-            err_sum_y_ = err_sum_y_ + err_y * dt_;
-        }
-        
-        // PI output (desired tilt in rad)
-        // Normalized wrt max values
-        double u_pitch = (kp_xy_ * err_x + ki_xy_ * err_sum_x_);
-        double u_roll  = (kp_xy_ * err_y + ki_xy_ * err_sum_y_);
-        int sgn_u_pitch = sgn(u_pitch);
-        int sgn_u_roll = sgn(u_roll);
-
-        // Anti-windup
-        bool sgn_in_matches_sgn_out_X = (sgn_error_x == sgn_u_pitch);
-        bool sgn_in_matches_sgn_out_Y = (sgn_error_y == sgn_u_roll);
-        double u_pitch_sat = std::clamp(u_pitch / max_tilt_angle_rad_, -1.0, 1.0);
-        double u_roll_sat = std::clamp(u_roll / max_tilt_angle_rad_, -1.0, 1.0);
-        is_saturated_x_ = (std::abs(u_pitch) >= max_tilt_angle_rad_);
-        is_saturated_y_ = (std::abs(u_roll) >= max_tilt_angle_rad_);
-        integrator_on_x_ = !(is_saturated_x_ && sgn_in_matches_sgn_out_X);
-        integrator_on_y_ = !(is_saturated_y_ && sgn_in_matches_sgn_out_Y);
-        
-        // Output
-        TwistMsg cmd_vel;
-        // Normalize
-        cmd_vel.linear.x = u_pitch_sat;
-        cmd_vel.linear.y = u_roll_sat;
-        cmd_vel.linear.z = std::clamp(target_vel_body.z() / max_vertical_speed_mps_,-1.0, 1.0);
-        cmd_vel.angular.z = std::clamp(target_vel_world_.angular.y, -1.0, 1.0); // Yaw rate command directly
-        
-        // Check that values are finite
-        if ( !std::isfinite(cmd_vel.linear.x) || 
-             !std::isfinite(cmd_vel.linear.y) || 
-             !std::isfinite(cmd_vel.linear.z) || 
-             !std::isfinite(cmd_vel.angular.z) ) 
-        {
-            RCLCPP_FATAL(this->get_logger(), "Controller produced infinite cmd_vel");
-            throw std::runtime_error("Controller produced infinite cmd_vel");
-        } else{
-            // Publish
-            cmd_vel_pub_->publish(cmd_vel);
-        }
-    }
-
-    void stopDrone() {
-        err_sum_x_ = 0.0;
-        err_sum_y_ = 0.0;
-
-        // Send zero cmd_vel
-        TwistMsg cmd_vel_zero;
-        cmd_vel_zero.linear.x = 0.0;
-        cmd_vel_zero.linear.y = 0.0;
-        cmd_vel_zero.linear.z = 0.0;
-        cmd_vel_zero.angular.z = 0.0;
-        cmd_vel_pub_->publish(cmd_vel_zero);
-    }
-
-    // Subscribers
-    rclcpp::Subscription<OdomMsg>::SharedPtr odom_sub_;
-    rclcpp::Subscription<TwistMsg>::SharedPtr des_vel_sub_;
-    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr bebop_mode_sub_;
-
-    // Publishers
-    rclcpp::Publisher<TwistMsg>::SharedPtr cmd_vel_pub_;
 };
 
 int main(int argc, char * argv[]) {
